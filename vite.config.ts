4b7aa78ea19d4965d58bc35c12625643d6c2d365
ballyhoos <scott@ballyhoos.com.au>
2026-04-01 09:52:26 +1100

    Add build version cache busting for CSS and JS assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 
+const buildVersion = `${Date.now()}`;
+
 export default defineConfig({
   base: "/investments/",
+  plugins: [
+    {
+      name: "inject-build-version",
+      transformIndexHtml(html) {
+        return html.replaceAll("__APP_BUILD_VERSION__", buildVersion);
+      },
+    },
+  ],
 });
-
